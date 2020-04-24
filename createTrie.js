128837ab3741902ad72b3bae5418e5a45ce37d6a
Prateek Desai <prateekdesai1990@gmail.com>
2020-04-24 13:44:57 +0530

    Update createTrie.js

--- createTrie.js
+++ createTrie.js
@@ -1,4 +1,4 @@
-function buildTrie() {
+function buildTrie(words) {
     const root = {};
     for (let w of words) {
       let node = root;
@@ -10,3 +10,20 @@ function buildTrie() {
     }
     return root;
   }
+
+function search(root, word) {
+    let node = root;
+    for (c of word) {
+        if(node[c]) {
+            node = node[c];
+            if(node && node.word && node.word === word) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+var words = ["oath","oeap","oeat","rain"]
+var root = buildTrie(words);
+var res = search(root, word);
