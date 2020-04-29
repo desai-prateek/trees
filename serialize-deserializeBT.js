46cf72bf46f3233e8eb37e3fa7acb54c11bc8fe8
Prateek Desai <prateekdesai1990@gmail.com>
2020-04-29 18:38:06 +0530

    Create serialize-deserializeBT.js

---
+++ serialize-deserializeBT.js
@@ -0,0 +1,68 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * Encodes a tree to a single string.
+ *
+ * @param {TreeNode} root
+ * @return {string}
+ */
+var serialize = function(root) {
+    const treeArray = [];
+    preOrder(root, treeArray);
+    return treeArray.join(',');
+
+    function preOrder(root, array) {
+        if(!root){
+            array.push('null');
+            return;
+        }
+
+        array.push(root.val);
+        preOrder(root.left, array);
+        preOrder(root.right, array);
+    }
+};
+
+/**
+ * Decodes your encoded data to tree.
+ *
+ * @param {string} data
+ * @return {TreeNode}
+ */
+var deserialize = function(data) {
+    const treeArray = data.split(',');
+    return preOrder(treeArray);
+
+    function preOrder(array) {
+
+        const string = array.shift();
+        const root = string == 'null' ? null : new TreeNode(parseInt(string));
+        if(root){
+            root.left = preOrder(array);
+            root.right = preOrder(array);
+        }
+        return root;
+    }
+};
+
+/**
+ * Your functions will be called as such:
+ * deserialize(serialize(root));
+ */
+ 
+ 
+ You may serialize the following tree:
+
+    1
+   / \
+  2   3
+     / \
+    4   5
+
+as "[1,2,3,null,null,4,5]"
